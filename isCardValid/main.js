a6523357d5ff2a76a0c055a4fad425bab899d0e4
Roman Slyshynsky <slyshynsky94@gmail.com>
2017-09-08 10:18:32 +0300

    Added TravisCI

--- isCardValid/main.js
+++ isCardValid/main.js
@@ -15,24 +15,19 @@ function isCreditCardValid(str) {
         cardNumber = cardNumber.replace(/-/g, '');
 
         var arr = cardNumber.match(/\d/g);
-        console.log("arr: " + arr);
 
         var sortArr = arr.sort();
-        console.log("sortArr: " + sortArr);
         if (sortArr[0] === sortArr[sortArr.length - 1]) {
             card.valid = false;
             card.error = 'only one type of number';
             return card;
         }
-        console.log("arr: " + arr);
+		
         var sum = 0;
 
         for (var i = 0; i < arr.length; i++) {
             sum = sum + +arr[i];
-            console.log("sum " + i + ': ' + sum);
         }
-
-        console.log("FinSum: " + sum);
 
         if(sum < 16) {
             card.valid = false;
@@ -43,7 +38,6 @@ function isCreditCardValid(str) {
         var checkOdd = cardNumber.match(/\d/g);
 
         var finalNumber = parseInt(checkOdd[checkOdd.length - 1]);
-        console.log("FinalNumber: " + finalNumber);
         if(finalNumber % 2 !== 0) {
             card.valid = false;
             card.error = 'odd final number';
