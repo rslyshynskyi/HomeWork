133545d5bc417a0c4ed676c157bb5f5447c49fc7
Roman Slyshynsky <slyshynsky94@gmail.com>
2017-09-10 19:25:29 +0300

    Modified isCreditCardValid function

--- isCardValid/main.js
+++ isCardValid/main.js
@@ -3,26 +3,40 @@ function isCreditCardValid(str) {
 
     var card = { number: cardNumber, valid: true };
 
-    var check = /\b\d{4}-\d{4}-\d{4}-\d{4}\b/;
+    var checkForInvalidChars = function () {
+        var check = /\b\d{4}-\d{4}-\d{4}-\d{4}\b/;
 
-    if (!check.test(card.number)) {
-        card.valid = false;
-        card.error = 'invalid characters';
+        if (!check.test(card.number)) {
+            card.valid = false;
+            card.error = 'invalid characters';
+            return card;
+        }
+
+        return card
+    }
+
+    var checkNumberLength = function () {
+        if(card.number.length !== 19) {
+            card.valid = false;
+            card.error = 'wrong_length';
+            return card;
+        }
+
         return card;
     }
 
-    if (check.test(cardNumber)) {
-        cardNumber = cardNumber.replace(/-/g, '');
-
-        var arr = cardNumber.match(/\d/g);
-
-        var sortArr = arr.sort();
-        if (sortArr[0] === sortArr[sortArr.length - 1]) {
+    var checkForOneTypeNumber = function (arr) {
+        arr.sort();
+        if (arr[0] === arr[arr.length - 1]) {
             card.valid = false;
             card.error = 'only one type of number';
             return card;
         }
-		
+
+        return card;
+    };
+
+    var checkSumOfNumber = function (arr) {
         var sum = 0;
 
         for (var i = 0; i < arr.length; i++) {
@@ -35,21 +49,47 @@ function isCreditCardValid(str) {
             return card;
         }
 
-        var checkOdd = cardNumber.match(/\d/g);
+        return card;
+    };
 
-        var finalNumber = parseInt(checkOdd[checkOdd.length - 1]);
+    var checkFinalNumberForOdd = function (arr) {
+        var finalNumber = parseInt(arr[arr.length - 1]);
         if(finalNumber % 2 !== 0) {
             card.valid = false;
             card.error = 'odd final number';
+            return card;
         }
+
+        return card;
+    };
+
+    card = checkNumberLength();
+    if ( card.valid === false ) {
+        return card;
     }
-    else if(card.number.length !== 19) {
-        card.valid = false;
-        card.error = 'wrong_length';
+
+    card = checkForInvalidChars();
+    if ( card.valid === false) {
+        return card;
     }
-    else {
-        card.valid = false;
-        card.error = 'invalid characters';
+
+    cardNumber = card.number.replace(/-/g, '');
+
+    var arr = cardNumber.match(/\d/g);
+
+    card = checkForOneTypeNumber(arr);
+    if (card.valid === false) {
+        return card;
+    }
+
+    card = checkSumOfNumber(arr);
+    if(card.valid === false) {
+        return card;
+    }
+
+    card = checkFinalNumberForOdd(arr);
+    if (card.valid === false) {
+        return card;
     }
 
     return card;
